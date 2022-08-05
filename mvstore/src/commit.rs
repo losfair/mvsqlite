5792d29eb67df13f1a274c667416f6cc475bc7e8
Heyang Zhou <zhy20000919@hotmail.com>
2022-08-05 20:17:51 +0800

    Fix correctness of multi-phase commit. (#11)

--- mvstore/src/commit.rs
+++ mvstore/src/commit.rs
@@ -1,10 +1,20 @@
-use std::time::SystemTime;
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    time::SystemTime,
+};
 
 use anyhow::Result;
-use foundationdb::{options::MutationType, FdbError};
+use foundationdb::{
+    options::{ConflictRangeType, MutationType, TransactionOption},
+    FdbError,
+};
+use futures::stream::FuturesOrdered;
+use futures::StreamExt;
 use rand::RngCore;
 
 use crate::server::{generate_suffix_versionstamp_atomic_op, ContentIndex, Server};
+
+pub static COMMIT_MULTI_PHASE_THRESHOLD: AtomicUsize = AtomicUsize::new(1000);
 
 pub enum CommitResult {
     Committed { versionstamp: [u8; 10] },
@@ -24,10 +34,17 @@ impl Server {
     pub async fn commit<'a>(&self, ctx: CommitContext<'a>) -> Result<CommitResult> {
         // Begin the writes.
         // We do three-phase commit for large transactions here.
-        let multi_phase = ctx.index_writes.len() >= 1000;
+        let multi_phase =
+            ctx.index_writes.len() >= COMMIT_MULTI_PHASE_THRESHOLD.load(Ordering::Relaxed);
         let commit_token_key = self.construct_ns_commit_token_key(ctx.ns_id);
         let mut commit_token = [0u8; 16];
         rand::thread_rng().fill_bytes(&mut commit_token);
+        tracing::debug!(
+            multi_phase = multi_phase,
+            commit_token = hex::encode(&commit_token),
+            size = ctx.index_writes.len(),
+            "entering commit"
+        );
 
         // Phase 1 - Idempotency && Check page existence
         let mut txn = self.db.create_trx()?;
@@ -54,10 +71,15 @@ impl Server {
             None => {}
         }
 
+        let mut phase_1_ci_get_futures = FuturesOrdered::new();
+
         for (_, page_hash) in ctx.index_writes {
             let content_index_key = self.construct_contentindex_key(ctx.ns_id, *page_hash);
-            let data = txn.get(&content_index_key, false).await?;
-            if data.is_none() {
+            phase_1_ci_get_futures.push(txn.get(&content_index_key, false));
+        }
+
+        while let Some(data) = phase_1_ci_get_futures.next().await {
+            if data?.is_none() {
                 return Ok(CommitResult::BadPageReference);
             }
         }
@@ -75,6 +97,7 @@ impl Server {
             {
                 anyhow::bail!("commit interrupted before phase 2");
             }
+            tracing::debug!(commit_token = hex::encode(&commit_token), "commit phase 2");
         }
 
         // Phase 2 - content index insertion
@@ -82,27 +105,7 @@ impl Server {
         let now = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap();
-        for (_, page_hash) in ctx.index_writes {
-            let key = self.construct_contentindex_key(ctx.ns_id, *page_hash);
-            let value = ContentIndex::generate_mutation_payload(now);
-            txn.atomic_op(&key, &value, MutationType::SetVersionstampedValue);
-        }
 
-        if multi_phase {
-            txn = txn.commit().await.map_err(|e| FdbError::from(e))?.reset();
-            if txn
-                .get(&commit_token_key, false)
-                .await?
-                .as_ref()
-                .map(|x| &x[..])
-                .unwrap_or_default()
-                != commit_token
-            {
-                anyhow::bail!("commit interrupted before phase 3");
-            }
-        }
-
-        // Phase 3 - content insertion
         if let Some(md) = ctx.metadata {
             let metadata_key = self.construct_nsmd_key(ctx.ns_id);
             txn.set(&metadata_key, md.as_bytes());
@@ -111,13 +114,33 @@ impl Server {
         for (page_index, page_hash) in ctx.index_writes {
             let page_key_template = self.construct_page_key(ctx.ns_id, *page_index, [0u8; 10]);
             let page_key_atomic_op = generate_suffix_versionstamp_atomic_op(&page_key_template);
+            let ci_key = self.construct_contentindex_key(ctx.ns_id, *page_hash);
+            let ci_atomic_op = ContentIndex::generate_mutation_payload(now);
+            if multi_phase {
+                txn.set_option(TransactionOption::NextWriteNoWriteConflictRange)?;
+            }
             txn.atomic_op(
                 &page_key_atomic_op,
                 page_hash,
                 MutationType::SetVersionstampedKey,
             );
+            if multi_phase {
+                txn.set_option(TransactionOption::NextWriteNoWriteConflictRange)?;
+            }
+            txn.atomic_op(&ci_key, &ci_atomic_op, MutationType::SetVersionstampedValue);
         }
-
+        if multi_phase {
+            txn.add_conflict_range(
+                &self.construct_page_key(ctx.ns_id, std::u32::MIN, [0u8; 10]),
+                &self.construct_page_key(ctx.ns_id, std::u32::MAX, [0xffu8; 10]),
+                ConflictRangeType::Write,
+            )?;
+            txn.add_conflict_range(
+                &self.construct_contentindex_key(ctx.ns_id, [0u8; 32]),
+                &self.construct_contentindex_key(ctx.ns_id, [0xffu8; 32]),
+                ConflictRangeType::Write,
+            )?;
+        }
         let mut last_write_version_atomic_op_value = [0u8; 16 + 10 + 4];
         last_write_version_atomic_op_value[0..16].copy_from_slice(&ctx.idempotency_key);
         last_write_version_atomic_op_value[26..30].copy_from_slice(&16u32.to_le_bytes()[..]);
