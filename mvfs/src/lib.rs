aede94c1c993060c1b0a2c9601b56cb2d0d8f91f
losfair <zhy20000919@hotmail.com>
2022-08-20 16:03:35 +0000

    Separate mvfs into its own crate

---
+++ mvfs/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod types;
+pub mod vfs;
+pub use vfs::{Connection, MultiVersionVfs};
---
+++ mvfs/src/types.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LockKind {
+    None,
+    Shared,
+    Reserved,
+    Pending,
+    Exclusive,
+}
+
+impl LockKind {
+    pub fn level(self) -> u32 {
+        match self {
+            LockKind::None => 0,
+            LockKind::Shared => 1,
+            LockKind::Reserved => 2,
+            LockKind::Pending => 3,
+            LockKind::Exclusive => 4,
+        }
+    }
+}
